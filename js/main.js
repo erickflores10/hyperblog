1b571d3dcb79d6beca666294aa34e64b3e4f61d0
erickflores10 <developerwebmythos@gmail.com>
2021-10-12 17:20:22 -0500

    Estructura con html, css y js base

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Captura del elemento con id
+/*
+const headerTheme = document.getElementByClassName('.header');
+const pressButton = document.getElementById('btn');
+
+headerTheme.addEventListener('click', () => {
+    headerTheme.style.background = '#333';
+});
+*/
